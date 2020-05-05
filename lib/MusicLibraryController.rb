40d47e255d7c7c1857a009f0a10a7cf8fc4a35f7
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-05 15:04:36 +0000

    Automatically backed up by Learn

---
+++ lib/MusicLibraryController.rb
@@ -0,0 +1,11 @@
+class MusicLibraryController
+
+  def initialize(path='./db/mp3s')
+    @path = path
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+  end
+end
